362c28af062a6d52ca9b04f7e6fe8ee8e6bad1e9
Ioan-Emanuel Grama <gramai.emanuel@Ioan-Emanuels-MacBook-Pro.local>
2022-01-10 09:50:01 +0200

    Added seq for create users

--- services/user-service/src/main/resources/db/structure.sql
+++ services/user-service/src/main/resources/db/structure.sql
@@ -8,3 +8,27 @@ create table bank_users
     last_updated_on  TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+create sequence seq start 1;
+
+create or replace function insert_users (users_count integer) returns void as $$
+DECLARE
+    id_client_curent INTEGER := 1;
+    seq_start INTEGER := 1;
+begin
+    id_client_curent := nextval('seq');
+    seq_start := id_client_curent;
+    loop
+        raise notice '% % %', id_client_curent, seq_start, users_count;
+        if (id_client_curent - seq_start <= users_count) then
+            execute format($fmt$ insert into bank_users(id, email, iban, available_amount, created_on, last_updated_on)
+   	values (%1$L::varchar(36), concat('test_user_', %1$L::varchar(36), '@gmail.com'), concat('RO49AAAA1B310000000', %1$L::varchar(36)), %1$L::varchar(36), now(), now())$fmt$, id_client_curent);
+            id_client_curent := nextval('seq');
+        else
+            return;
+        end if;
+    end loop;
+end $$ language 'plpgsql';
+
+
+select insert_users(10000);
